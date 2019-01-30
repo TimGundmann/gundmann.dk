0fc4e6d69a3a5b63028939d2371a30e1ea5744e9
Tim Gundmann <tim@gundmann.dk>
2019-01-30 14:18:01 +0100

    move things

--- src/app/user/signin/signin.component.spec.ts
+++ src/app/user/signin/signin.component.spec.ts
@@ -1,4 +1,4 @@
-import { SigninService } from './../signin.service';
+import { SigninService } from '../../services/signin.service';
 import { FormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
--- src/app/user/signin/signin.component.ts
+++ src/app/user/signin/signin.component.ts
@@ -1,7 +1,6 @@
-import { SigninService } from './../signin.service';
+import { SigninService } from '../../services/signin.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-signin',
